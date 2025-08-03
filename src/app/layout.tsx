914582329ae5be64840aaa019a4a6b381493a2f9
Haris Farooq <harisnetbackup@gmail.com>
2025-08-03 10:01:29 +0000

    bigger gradeiend spheres please

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,9 +23,9 @@ export default function RootLayout({
       </head>
       <body className="font-body antialiased min-h-screen bg-background flex flex-col relative">
         <div className="absolute top-0 left-0 -z-10">
-            <div className="absolute top-[30vh] left-[5vw] h-72 w-72 rounded-full bg-gradient-to-br from-blue-500 to-purple-600 opacity-20 blur-3xl filter" />
-            <div className="absolute top-[20vh] right-[10vw] h-52 w-52 rounded-full bg-gradient-to-br from-blue-400 to-purple-500 opacity-60 blur-2xl filter" />
-            <div className="absolute top-[60vh] left-[30vw] h-40 w-40 rounded-full bg-gradient-to-br from-blue-400 to-purple-500 opacity-50 blur-2xl filter" />
+            <div className="absolute top-[20vh] left-[-5vw] h-[44rem] w-[44rem] rounded-full bg-gradient-to-br from-blue-500 to-purple-600 opacity-20 blur-3xl filter" />
+            <div className="absolute top-[10vh] right-[5vw] h-[34rem] w-[34rem] rounded-full bg-gradient-to-br from-blue-400 to-purple-500 opacity-60 blur-2xl filter" />
+            <div className="absolute top-[50vh] left-[25vw] h-[28rem] w-[28rem] rounded-full bg-gradient-to-br from-blue-400 to-purple-500 opacity-50 blur-2xl filter" />
         </div>
         <Header />
         <main className="flex-grow z-10">{children}</main>
